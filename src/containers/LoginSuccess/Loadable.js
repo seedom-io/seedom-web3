df88b11019b0efffa04f987120c559898eec6a8c
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-01-06 13:36:41 -0700

    remove a bunch of cruft

--- src/containers/LoginSuccess/Loadable.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Loadable from 'react-loadable';
-
-const LoginSuccessLoadable = Loadable({
-  loader: () => import('./LoginSuccess'),
-  loading: () => <div>Loading</div>
-});
-
-export default LoginSuccessLoadable;
