662c9be23494967376c1239ea8099376fee42896
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-01-06 14:41:01 -0700

    add bulma

--- src/containers/Seedom/Loadable.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Loadable from 'react-loadable';
-
-const SeedomLoadable = Loadable({
-  loader: () => import('./Seedom'),
-  loading: () => <div>Loading</div>
-});
-
-export default SeedomLoadable;
