6908f485a9d33da158395cac4a670d9cd439dcd4
Alex <agroleau@salesforce.com>
2018-01-06 19:45:42 -0500

    progress

---
+++ app/components/Puck/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Progress extends Component {
+    render() {
+        return (
+            <div>HELLO</div>
+        );
+    }
+}
+  
+export default Progress;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from '../../components/Button';
+import Puck from '../../components/Puck';
 
 const HomePage = () => (
   <div>
     <h2>Welcome to Seedom</h2>
     <Button />
+    <Puck />
   </div>
 )
 
