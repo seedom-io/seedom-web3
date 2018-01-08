1c435a1c3245a5b2647ccb221ea1b63d07ec7760
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-01-07 20:19:31 -0700

    set raiser to seedom puck

--- app/containers/HomePage/components/LoadedHomePage/index.js
+++ app/containers/HomePage/components/LoadedHomePage/index.js
@@ -4,6 +4,13 @@ import { rpcWeb3, wsWeb3 } from '../../../../utils/web3';
 import testJSON from '../../../../../../seedom-solidity/deployment/test.json';
 
 class LoadedHomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      raiser: null
+    };
+  }
+
   componentWillMount() {
     const contractAddress = testJSON.seedom[0].address;
 
@@ -19,10 +26,25 @@ class LoadedHomePage extends React.Component {
       from: account,
       gasPrice: '20000000000' // default gas price in wei, 20 gwei in this case
     });
+
+    wsContract.methods
+      .currentRaiser()
+      .call({
+        from: account
+      })
+      .then(
+        raiser => {
+          this.setState({ raiser })
+        },
+        err => {
+          console.error(err)
+        }
+      );
   }
 
   render() {
     const { account } = this.props;
+    const { raiser } = this.state;
 
     return (
       <div>
@@ -30,7 +52,7 @@ class LoadedHomePage extends React.Component {
         <p>
           <strong>Account:</strong> {account}
         </p>
-        <SeedomPuck />
+        {raiser && <SeedomPuck raiser={raiser} />}
       </div>
     );
   }
