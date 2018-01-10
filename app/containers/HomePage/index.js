52689864a4d913d593175359ec1823ac1313834e
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-01-09 20:03:09 -0700

    black like my heart

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -53,7 +53,7 @@ class HomePage extends React.Component {
 
     return (
       <div>
-        <section className="hero is-info is-fullheight">
+        <section className="hero is-fullheight">
           <div className="hero-body">
             <div className="container has-text-centered">
               {contractLoaded &&
