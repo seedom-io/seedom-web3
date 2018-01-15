6bbb48422cbebbefef7181c74ed951d3295c95d2
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-01-15 14:55:24 -0700

    replace logo

--- app/components/Nav/index.js
+++ app/components/Nav/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../../img/logos/seedom-white@4x.png';
 
 const Nav = () => (
   <nav className="navbar">
     <div className="container">
       <div className="navbar-brand">
         <a className="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+          <img src={logo} alt="Seedom - Seeding the future of philanthropy" />
         </a>
         <span className="navbar-burger burger" data-target="navbarMenu">
           <span />
