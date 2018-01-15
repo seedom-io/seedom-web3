acf86045b209dfa8fbb53a7e2ba498cdbb41f918
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-01-15 13:20:12 -0700

    add source maps

--- private/webpack/dev.js
+++ private/webpack/dev.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge');
 const base = require('./base.js');
 
 module.exports = merge(base, {
+  devtool: 'eval-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, 'app'),
     port: 1234
