0e6d05579665425f609de8eb2cc84a88f63b1bb4
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-01-15 11:05:57 -0700

    move nav to component

---
+++ app/components/Nav/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const Nav = () => (
+  <nav className="navbar">
+    <div className="container">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+        </a>
+        <span className="navbar-burger burger" data-target="navbarMenu">
+          <span />
+          <span />
+          <span />
+        </span>
+      </div>
+      <div id="navbarMenu" className="navbar-menu">
+        <div className="navbar-end">
+          <a className="navbar-item" href="https://seedom-io.github.io/seedom-whitepaper/seedom-whitepaper.pdf">
+            Whitepaper
+          </a>
+          <span className="navbar-item">
+            <a className="button is-white is-outlined is-small" href="https://github.com/seedom/seedom-solidity/blob/master/contract/seedom.sol">
+              <span className="icon">
+                <i className="fa fa-github" />
+              </span>
+              <span>View Smart Contract</span>
+            </a>
+          </span>
+          <a className="navbar-item" href="https://medium.com/@seedom.io">
+            <span className="icon">
+              <i className="fa fa-lg fa-medium" />
+            </span>
+          </a>
+          <a className="navbar-item" href="http://reddit.com/r/seedom_io">
+            <span className="icon">
+              <i className="fa fa-lg fa-reddit" />
+            </span>
+          </a>
+          <a className="navbar-item" href="https://www.facebook.com/seedom.io">
+            <span className="icon">
+              <i className="fa fa-lg fa-facebook" />
+            </span>
+          </a>
+
+          <a className="navbar-item" href="https://twitter.com/seedom_io">
+            <span className="icon">
+              <i className="fa fa-lg fa-twitter" />
+            </span>
+          </a>
+
+          <a className="navbar-item" href="https://www.instagram.com/seedom.io/">
+            <span className="icon">
+              <i className="fa fa-lg fa-instagram" />
+            </span>
+          </a>
+
+          <a className="navbar-item" href="https://t.me/seedomio">
+            <span className="icon">
+              <i className="fa fa-lg fa-telegram" />
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Nav;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 
 import './App.scss';
+import Nav from '../../components/Nav';
 import HomePage from '../HomePage';
 
 const App = () => (
@@ -17,68 +18,7 @@ const App = () => (
       <meta name="description" content="Seedom - Seeding the future of philanthropy" />
     </Helmet>
     <div className="hero-head">
-      <nav className="navbar">
-        <div className="container">
-          <div className="navbar-brand">
-            <a className="navbar-item" href="https://bulma.io">
-              <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
-            </a>
-            <span className="navbar-burger burger" data-target="navbarMenu">
-              <span />
-              <span />
-              <span />
-            </span>
-          </div>
-          <div id="navbarMenu" className="navbar-menu">
-            <div className="navbar-end">
-              <a className="navbar-item" href="https://seedom-io.github.io/seedom-whitepaper/seedom-whitepaper.pdf">
-                Whitepaper
-              </a>
-              <span className="navbar-item">
-                <a className="button is-white is-outlined is-small" href="https://github.com/seedom/seedom-solidity/blob/master/contract/seedom.sol">
-                  <span className="icon">
-                    <i className="fa fa-github" />
-                  </span>
-                  <span>View Smart Contract</span>
-                </a>
-              </span>
-              <a className="navbar-item" href="https://medium.com/@seedom.io">
-                <span className="icon">
-                  <i className="fa fa-lg fa-medium" />
-                </span>
-              </a>
-              <a className="navbar-item" href="http://reddit.com/r/seedom_io">
-                <span className="icon">
-                  <i className="fa fa-lg fa-reddit" />
-                </span>
-              </a>
-              <a className="navbar-item" href="https://www.facebook.com/seedom.io">
-                <span className="icon">
-                  <i className="fa fa-lg fa-facebook" />
-                </span>
-              </a>
-
-              <a className="navbar-item" href="https://twitter.com/seedom_io">
-                <span className="icon">
-                  <i className="fa fa-lg fa-twitter" />
-                </span>
-              </a>
-
-              <a className="navbar-item" href="https://www.instagram.com/seedom.io/">
-                <span className="icon">
-                  <i className="fa fa-lg fa-instagram" />
-                </span>
-              </a>
-
-              <a className="navbar-item" href="https://t.me/seedomio">
-                <span className="icon">
-                  <i className="fa fa-lg fa-telegram" />
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Nav />
     </div>
     <Router>
       <div>
