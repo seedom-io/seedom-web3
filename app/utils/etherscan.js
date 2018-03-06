20487794d6eb01c64601398b35e85e443fbf1661
Alex <agroleau@salesforce.com>
2018-03-06 17:12:52 -0500

    fixed etherscan links

--- app/utils/etherscan.js
+++ app/utils/etherscan.js
@@ -1,8 +1,14 @@
 const getSubdomain = (network) => ((network === 'mainnet') ? 'www' : network);
 
-const getAddressUrl = (network, address) => `https://${getSubdomain(network)}.etherscan.io/address/${address}`;
+const getAddressUrl = (network, address) => {
+  if (network === 'localhost') { return ''; }
+  return `https://${getSubdomain(network)}.etherscan.io/address/${address}`;
+};
 
-const getTransactionUrl = (network, transactionHash) => `https://${getSubdomain(network)}.etherscan.io/tx/${transactionHash}`;
+const getTransactionUrl = (network, transactionHash) => {
+  if (network === 'localhost') { return ''; }
+  return `https://${getSubdomain(network)}.etherscan.io/tx/${transactionHash}`;
+};
 
 export {
   getAddressUrl,
