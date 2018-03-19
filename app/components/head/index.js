c71a0ee1cf2f44ac1e9b0bf971da0f025c56c29b
Alex <agroleau@salesforce.com>
2018-03-19 11:34:17 -0400

    added gitter sidecar widget

--- app/components/head/index.js
+++ app/components/head/index.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 
 class Head extends Component {
+  componentDidMount() {
+    ((window.gitter = {}).chat = {}).options = {
+      room: 'seedom_io/Lobby'
+    };
+  }
+
   render() {
     return (
       <Helmet titleTemplate="%s - Seedom" defaultTitle="Seedom">
         <meta name="description" content="Seedom - Seeding the future of philanthropy" />
         <meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=0.7" />
+        <script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer />
       </Helmet>
     );
   }
