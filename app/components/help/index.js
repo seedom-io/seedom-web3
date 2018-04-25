75f1150eb1d551005ca5fe4dd997bbd9b7075a5b
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-04-24 22:18:58 -0600

    Create toggle component (basically the same as collapse) for handling the help section.

--- app/components/help/index.js
+++ app/components/help/index.js
@@ -1,54 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Sections from '../sections';
-import Collapse from '../collapse';
+import Toggle from '../toggle';
 import './index.scss';
 
 class Help extends Sections {
   render() {
     const { open } = this.state;
+
     return (
       <div className="seedom-help">
-
-        <Collapse
-          title="accessing seedom on mobile"
-          collapsed={!open.includes('accessing-mobile')}
-        >
-          <div className="section">
-            Hello crazy mobile section!
+        <div className="container">
+          <div className="column is-three-fifths is-offset-one-fifth">
+            <h3 className="title has-text-white">Accessing Seedom</h3>
+            <Toggle
+              title="Accessing on mobile"
+              collapsed={!open.includes('accessing-mobile')}
+            >
+              To use Seedom on mobile, we recommend the <a target="_blank" href="https://www.cipherbrowser.com/">Cipher app</a>. You will need to put Ether into your Cipher wallet in order to participate.
+            </Toggle>
+            <Toggle
+              title="Accessing on desktop"
+              collapsed={!open.includes('accessing-desktop')}
+            >
+              To use Seedom on the desktop, you will need to install <a target="_blank" href="https://metamask.io/">>MetaMask</a> or use the <a target="_blank" href="https://brave.com/">Brave browser</a>. You will need to put Ether into your MetaMask wallet in order to participate.
+            </Toggle>
           </div>
-        </Collapse>
-
-        <Collapse
-          title="accessing seedom on desktop"
-          collapsed={!open.includes('accessing-desktop')}
-        >
-          <div className="section">
-
-          </div>
-        </Collapse>
-
-        <Collapse
-          title="obtaining ether"
-          collapsed={!open.includes('obtaining-ether')}
-        >
-          <div>HELLO!</div>
-        </Collapse>
-
-        <Collapse
-          title="participating in seedom"
-          collapsed={!open.includes('participating')}
-        >
-          <div>HELLO!</div>
-        </Collapse>
-
-        <Collapse
-          title="voting for future causes"
-          collapsed={!open.includes('voting')}
-        >
-          <div>HELLO!</div>
-        </Collapse>
-
+        </div>
       </div>
     );
   }
