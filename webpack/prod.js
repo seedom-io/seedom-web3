9a9282c7e765ca84f9fee13571d5a634a1ddc9ac
Alexander Groleau <awgneo@Xerxes-300.local>
2018-11-12 14:16:36 -0500

    no prod debug

--- webpack/prod.js
+++ webpack/prod.js
@@ -3,15 +3,15 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const loader = require('../../seedom-solidity/chronicle/loader');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'eval',
+  mode: 'production',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      'process.env.NODE_ENV': JSON.stringify('production'),
       ETH_NETWORKS: JSON.stringify(loader.getNetworks()),
       ETH_DEPLOYMENTS: JSON.stringify(loader.getDeployments()),
       SEEDOM_URL: JSON.stringify('https://seedom.io'),
       BADGER_URL: JSON.stringify('https://seedom.io')
-    })
+    }),
+    new UglifyJSPlugin()
   ]
 };
